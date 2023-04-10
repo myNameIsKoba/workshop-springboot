8e6301cbc0176ed98d976e4fef3576c85a535375
myNameIsKoba <gykobayashi89@gmail.com>
2023-04-10 12:10:20 -0300

    Camadas de serviços e registro de componentes

---
+++ src/main/java/com/myApp/main/Services/UserService.java
@@ -0,0 +1,27 @@
+package com.myApp.main.Services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.myApp.main.entities.User;
+import com.myApp.main.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repositorio;
+	
+	public List<User> findAll(){
+		return repositorio.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repositorio.findById(id);
+		
+		return obj.get();
+	}
+}
